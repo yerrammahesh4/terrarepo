acde58db9a72fca53a62689bc19e4c8cf4210cd3
RAHAMSHAIK007 <81603735+RAHAMSHAIK007@users.noreply.github.com>
2023-02-01 10:21:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIASBZHGLZN6LBZZFF6"
-secret_key = "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"
+access_key = "*****************"
+secret_key = "****************************"
 }
 
 resource "aws_instance" "one" {
